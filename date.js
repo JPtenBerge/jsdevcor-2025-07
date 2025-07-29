8d6f212fa7cf18a36b1910dda89e872816160ae6
JP <jp@jp.nl>
2025-07-29 11:01:29 +0200

    Objects laten zien

---
+++ date.js
@@ -0,0 +1,17 @@
+
+let nu = new Date('1729-04-03T12:00:00Z');
+console.log(nu.toLocaleString());
+
+console.log(nu.getDate());
+console.log(nu.getMonth() + 1);
+console.log(nu.getFullYear());
+
+let toen1 = new Date("2012-03-13");
+let toen2 = new Date("2012-3-13");
+console.log(toen1);
+console.log(toen2);
+
+// toen1.
+
+class A {}
+class B extends A {} // syntactic sugar prototypes
---
+++ objects.js
@@ -0,0 +1,33 @@
+
+let obj = {
+    x: 24,
+    x: 42,
+    y: 'hoi',
+    z: {},
+    q: []
+};
+obj['vet gave properynamen ❤😂😎🎂'] = 'huh';
+console.log(obj);
+console.log(obj.y);
+console.log(obj['y']);
+console.log(obj['vet gave properynamen ❤😂😎🎂'])
+
+
+let obj2 = new Object();
+obj2.ditkan = 42;
+console.log(obj2);
+
+// built-in objects
+// JSON
+// globale
+// Date
+// Math
+// Number
+// Object
+// Array
+
+
+
+let nu = new Date();
+nu.whatever = 'hoi';
+console.log(nu);
